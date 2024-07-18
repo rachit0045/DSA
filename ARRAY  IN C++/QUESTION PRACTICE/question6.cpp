8440c9aa8ed2f23f24c35e8cbef4cb644dcaa4c8
rachit0045 <rachitsharma9022@gmail.com>
2024-07-18 10:43:39 +0530

    program to count the number of element in array which is greater than x element

---
+++ ARRAY  IN C++/QUESTION PRACTICE/question6.cpp
@@ -0,0 +1,30 @@
+// Count the number of elements in given array greater than a given number x
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, x;
+    cout << "Enter the size of array : ";
+    cin >> n;
+    cout << "Enter the element of array : ";
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the number x: ";
+    cin >> x;
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > x)
+        {
+            count++;
+        }
+    }
+
+    cout << "Number of elements greater than " << x << " is: " << count << endl;
+    return 0;
+}
